412e81a3a26d634fccf443e1d490cb87d099fb80
HamzaBacha <54685154+HamzaBacha@users.noreply.github.com>
2025-05-26 15:40:12 +0300

    Task2 done

--- appointmentservice/src/main/java/com/esi/appointmentservice/service/AppointmentService.java
+++ appointmentservice/src/main/java/com/esi/appointmentservice/service/AppointmentService.java
@@ -1,7 +1,13 @@
 package com.esi.appointmentservice.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.esi.appointmentservice.dto.AppointmentDto;
+import com.esi.appointmentservice.model.Appointment;
+import com.esi.appointmentservice.repository.AppointmentRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,8 +16,30 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class AppointmentService {
 
-  public void appointmentBooked(AppointmentDto appointmentDto) {
+  @Autowired
+  private AppointmentRepository appointmentRepository;
 
+  public void appointmentBooked(AppointmentDto appointmentDto) {
+  
+    Appointment  appointment = Appointment.builder()
+        .appointmentid(appointmentDto.getAppointmentid())
+        .studentId(appointmentDto.getStudentId())
+        .studentName(appointmentDto.getStudentName())
+        .teacherId(appointmentDto.getTeacherId())
+        .date(appointmentDto.getDate())
+        .build();
+
+      
+        appointmentRepository.save(appointment);
+  }
+  private AppointmentDto mapToAppointmentDto(Appointment appointment) {
+    return AppointmentDto.builder()
+        .appointmentid(appointment.getAppointmentid())
+        .studentId(appointment.getStudentId())
+        .studentName(appointment.getStudentName())
+        .teacherId(appointment.getTeacherId())
+        .date(appointment.getDate())
+        .build();
   }
 
 }
